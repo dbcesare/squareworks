5bc471ee10db37f9419bc29ba0d0c1bdc0985c38
thatoldplatitude <itsfirstnamedotlastname@tutanota.com>
2024-06-24 12:43:21 -0400

    create submit handler. add redirect to forcast page while passing (currently) mocked data

--- squareworks/src/components/Address.js
+++ squareworks/src/components/Address.js
@@ -1,7 +1,9 @@
 import { useRef, useState, useEffect } from "react";
+import { redirect, useNavigate } from "react-router-dom";
 const addressFields = {address:"",city:"",state:"",zip:""};
 
 const Address = () => {
+    const navigate = useNavigate();
     const addressRef = useRef();
     const [addressFields, setAddressFields] = useState({address:"",city:"",state:"",zip:""});
     const [validMatch, setValidMatch] = useState(false);
@@ -9,10 +11,6 @@ const Address = () => {
     useEffect(() => {
         addressRef.current.focus();
     },[]);
-
-    // useEffect(() => {
-    //     console.log(addressFields);
-    // },[addressFields]);
 
 
     const handleChange = (l,e) => {
@@ -27,10 +25,51 @@ const Address = () => {
         }
         console.log(addressFields);
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Parse address fields and hit backend
+        // fetch("http://localhost:3600/sw-api/address");
+
+        // What the response will be looking like
+        const obj = {
+            "current": {
+                "time": "2024-06-24T00:15",
+                "interval": 900,
+                "temperature_2m": 71.0,
+                "precipitation": 0.000,
+                "weather_code": 3
+            },
+            "forcasts": [
+                {
+                    date:"2023-06-23",
+                    weatherCode: 65,
+                    minTemp: "63.4",
+                    maxTemp: "76.0"
+                },
+                {
+                    date:"2023-06-24",
+                    weatherCode: 3,
+                    minTemp: "65.7",
+                    maxTemp: "77.9"
+                },
+                {
+                    date:"2023-06-25",
+                    weatherCode: 51,
+                    minTemp: "40",
+                    maxTemp: "80.9"
+                }
+            ]
+        };
+
+        //with a successfull response, redirect to the page to display the weather forcast.
+        navigate("/forcast",{state:{...obj}});
+    }
+
     return (
         <section>
             <h1>Enter Address</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="address">Address</label>
                 <input
                     type="text"
@@ -67,6 +106,7 @@ const Address = () => {
                     value={addressFields.zip}
                     onChange={(e) => handleChange("zip",e)}
                 />
+                <input type="submit" />
             </form>
         </section>
     );
